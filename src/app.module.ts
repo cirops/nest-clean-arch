25879b0dbada54da7d7ff4f3facc038105315bee
cirops <cirops@gmail.com>
2024-07-28 21:18:35 -0300

    feat: Create a session using jwt

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,8 @@ import { CreateAccountController } from './controllers/create-account.controller
 import { ListAccountsController } from './controllers/list-accounts.controller'
 import { ConfigModule } from '@nestjs/config'
 import { envSchema } from './env'
+import { AuthModule } from './auth/auth.module'
+import { AuthenticateController } from './controllers/authenticate.controller'
 
 @Module({
   imports: [
@@ -11,8 +13,13 @@ import { envSchema } from './env'
       validate: (env) => envSchema.parse(env),
       isGlobal: true,
     }),
+    AuthModule,
   ],
-  controllers: [CreateAccountController, ListAccountsController],
+  controllers: [
+    CreateAccountController,
+    ListAccountsController,
+    AuthenticateController,
+  ],
   providers: [PrismaService],
 })
 export class AppModule {}
